8f90aa2b17f8a83a0383d0d873021ce02730d7cb
Daniel Gomez <51731637+danieldevelop@users.noreply.github.com>
2026-04-06 02:11:32 -0400

    se cambia linea de codigo para redireccionar a login

--- utils/auth.py
+++ utils/auth.py
@@ -1,10 +1,11 @@
 from functools import wraps
-from flask import session, jsonify
+from flask import session, jsonify, redirect, url_for
 
 def login_required(view_func):
     @wraps(view_func)
     def wrapper(*args, **kwargs):
         if not session.get("user_id"):
-            return jsonify({"ok": False, "message": "No autenticado"}), 401
+            # return jsonify({"ok": False, "message": "No autenticado"}), 401
+            return redirect(url_for("web.login"))
         return view_func(*args, **kwargs)
     return wrapper
