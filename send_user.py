8e3be71d23c74379161b9432b29d6147c2935aea
Daniel Gomez <51731637+danieldevelop@users.noreply.github.com>
2026-04-08 23:51:11 -0400

    se registra nuevo usuario

--- send_user.py
+++ send_user.py
@@ -3,11 +3,11 @@ from extensions import db
 from models.user import User
 
 with app.app_context():
-    exists = User.query.filter_by(username="daniel").first()
+    exists = User.query.filter_by(username="victor").first()
     if exists:
-        print("El usuario 'daniel' ya existe.")
+        print("El usuario 'victor' ya existe.")
     else:
-        u = User(username="daniel")
+        u = User(username="victor")
         u.set_password("Pass1234")
         db.session.add(u)
         db.session.commit()
